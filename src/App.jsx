5953186634861f74f711524e15da8eedd3e47690
Marco <marcoadrianzambra@gmail.com>
2025-02-07 11:08:48 -0500

    Board fill function using the click

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ const TURNS = {
     O: 'o'
 }
 
-const Square = ({children, isSelected, isTurnSquare, updateTurn}) => {
+const Square = ({children, isSelected, isTurnSquare, updateBoard, index}) => {
     const className = `square ${isTurnSquare ? 'turn' : ''} ${isSelected ? 'turn--is-selected' : ''}`
+
+    const handeClick = () => {
+        updateBoard(index)
+    }
     
     return (
-        <div className={className} onClick={updateTurn}>
+        <div className={className} onClick={handeClick}>
             {children}
         </div>
     )
@@ -21,8 +25,15 @@ function App() {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [turn, setTurn] = useState(TURNS.X);
 
-    const updateTurn = () => {
-        setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);
+    const updateBoard = (index) => {
+
+        if (board[index]) return;   // if the square is current filled, do nothing, return
+
+        setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);  //change turn state from X to O and vice versa 
+
+        const copyBoard = [...board];
+        copyBoard[index] = turn;
+        setBoard(copyBoard);
     }
 
     return (
@@ -32,8 +43,12 @@ function App() {
                 {
                     board.map( (_, index) => {
                         return (
-                            <Square key={index} updateTurn={updateTurn}>
-                                {index}
+                            <Square 
+                                key={index} 
+                                index={index}
+                                updateBoard={updateBoard}
+                            >
+                                {board[index]}
                             </Square>
                         )
                     })
@@ -41,10 +56,16 @@ function App() {
             </section>
 
             <section className='turn-container'>
-                <Square isSelected={ turn === TURNS.X } isTurnSquare>
+                <Square 
+                    isSelected={ turn === TURNS.X } 
+                    isTurnSquare
+                >
                     {TURNS.X}
                 </Square>
-                <Square isSelected={ turn === TURNS.O } isTurnSquare>
+                <Square 
+                    isSelected={ turn === TURNS.O } 
+                    isTurnSquare
+                >
                     {TURNS.O}
                 </Square>
             </section>
