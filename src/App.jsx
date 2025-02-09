c463ab93d5545d244fc10e2b68c0b2380ccf4702
Marco <marcoadrianzambra@gmail.com>
2025-02-08 23:54:23 -0500

    Winner detection

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ const TURNS = {
     O: 'o'
 }
 
+const winnerPatterns = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 4, 8],
+    [2, 4, 6], 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8] 
+]
+
+
+
 const Square = ({children, isSelected, isTurnSquare, updateBoard, index}) => {
     const className = `square ${isTurnSquare ? 'turn' : ''} ${isSelected ? 'turn--is-selected' : ''}`
 
@@ -24,16 +37,40 @@ function App() {
 
     const [board, setBoard] = useState(Array(9).fill(null));
     const [turn, setTurn] = useState(TURNS.X);
+    const [winner, setWinner] = useState(null)
+
+    const checkWinner = (newBoard) => {
+
+        for (const combo of winnerPatterns) {
+            const [a, b, c] = combo
+
+            if (
+                newBoard[a] &&
+                newBoard[a] === newBoard[b] &&
+                newBoard[a] === newBoard[c]
+            ) {
+                return newBoard[a]
+            }
+        }
+
+        // if no winner combo yet
+        return null
+
+    }
 
     const updateBoard = (index) => {
-
-        if (board[index]) return;   // if the square is current filled, do nothing, return
+        if (board[index] || winner) return;   // if the square is current filled, do nothing, return
 
         setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);  //change turn state from X to O and vice versa 
 
-        const copyBoard = [...board];
-        copyBoard[index] = turn;
-        setBoard(copyBoard);
+        const newBoard = [...board];
+        newBoard[index] = turn;
+        setBoard(newBoard);
+
+        const newWinner = checkWinner(newBoard);
+        if (newWinner) {
+            setWinner(newWinner)
+        }
     }
 
     return (
