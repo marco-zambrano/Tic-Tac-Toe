2dcae5b9c063be92f8781d7456fba2b752ae496d
Marco <marcoadrianzambra@gmail.com>
2025-02-10 18:51:53 -0500

    Apply Standard JS

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,13 +1,17 @@
 import { Square } from "./Square.jsx"
 import { Button } from "./Button.jsx"
 
+// eslint-disable-next-line react/prop-types
 export function WinnerModal({winner, visibility, changeVisibility, restartGame}) {
     if (winner === null || !visibility) return null
     
     return (
         <section className="modal-container">
             <button className="close-btn" onClick={changeVisibility}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="20" height="20" strokeWidth="1"> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="20" height="20" strokeWidth="1">
+                    <path d="M18 6l-12 12"></path> 
+                    <path d="M6 6l12 12"></path> 
+                </svg> 
             </button>
 
             {
