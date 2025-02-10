cd5e059e508bc8fdf368ae024e92896d71bc050a
Marco <marcoadrianzambra@gmail.com>
2025-02-09 20:09:32 -0500

    Close and open modal results func, and DOM optimitation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import './App.css'
 
 const TURNS = {
@@ -6,7 +6,7 @@ const TURNS = {
     O: 'o'
 }
 
-const winnerPatterns = [
+const winnerCombos = [
     [0, 1, 2], 
     [3, 4, 5], 
     [6, 7, 8], 
@@ -33,25 +33,33 @@ const Square = ({children, isSelected, isTurnSquare, updateBoard, index}) => {
     )
 }
 
+
+const Button = ({children, onClick, className}) => {
+    return (
+        <button onClick={onClick} className={className}>{children}</button>
+    )
+}
+
+
 function App() {
 
     const [board, setBoard] = useState(Array(9).fill(null));
     const [turn, setTurn] = useState(TURNS.X);
-    const [winner, setWinner] = useState(null)
+    const [winner, setWinner] = useState(null);
+    const [visibility, setVisibility] = useState(false);
 
     const restartGame = () => {
         setBoard(Array(9).fill(null));
         setTurn(TURNS.X);
         setWinner(null);
-
-
-
+        setVisibility(false);
     }
-    
-    
-    
+    const changeVisibility = () => {
+        setVisibility(!visibility);
+    }
+
     const checkWinner = (newBoard) => {
-        for (const combo of winnerPatterns) {
+        for (const combo of winnerCombos) {
             const [a, b, c] = combo
             if (
                 newBoard[a] &&
@@ -61,30 +69,31 @@ function App() {
                 return newBoard[a]
             }
         }
-        // if no winner combo yet
+        // Not a winner yet
         return null
     }
 
     const updateBoard = (index) => {
-        // if the square is current filled, do nothing, return
-        if (board[index] || winner) return;   
-        //change turn state from X to O and vice versa 
-        setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);  
+        if (board[index] || winner) return; // if the square is current filled, do nothing, return
+        
+        setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);  //change turn state from X to O and vice versa 
 
         const newBoard = [...board];
         newBoard[index] = turn;
-        setBoard(newBoard);
+        setBoard(newBoard); // Set the new board with the new value inserted
 
-        const newWinner = checkWinner(newBoard);
+        const newWinner = checkWinner(newBoard); // verify winner
         if (newWinner) {
-            setWinner(newWinner)
+            setWinner(newWinner);
+            changeVisibility()
         }
     }
+
 
     return (
         <main className='board'>
             <h1>Tic Tac Toe</h1>
-            <button onClick={restartGame} className="reset-btn">Restart Game?</button>
+            <Button onClick={restartGame} className={'reset-btn'}>Restart Game</Button>
             <section className='game'>
                 {
                     board.map( (_, index) => {
@@ -101,6 +110,12 @@ function App() {
                 }
             </section>
 
+            {
+                !visibility && winner !== null && (
+                    <Button onClick={changeVisibility} className={'reset-btn'}>Watch the result</Button>
+                )
+            }
+
             <section className='turn-container'>
                 <Square 
                     isSelected={ turn === TURNS.X } 
@@ -115,15 +130,18 @@ function App() {
                     {TURNS.O}
                 </Square>
             </section>
-
+            
             {
-                winner !== null && (
+                winner !== null && visibility &&(
                     <section className="modal-container">
+                        <button className="close-btn" onClick={changeVisibility}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="20" height="20" stroke-width="1"> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg> 
+                        </button>
                         <h2 className="dialog-text">Winner: </h2>
                         <div className="modal-square-container">
                             <Square>{winner}</Square>
                         </div>
-                        <button onClick={restartGame} className="reset-btn">Restart Game?</button>
+                        <Button onClick={restartGame} className={'reset-btn'}>Restart Game</Button>
                     </section>
                 )
             }
